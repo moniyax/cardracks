aa17ebed78a716fcff17a8d198fa8f7de6ede111
Mayowa Yakub <mayoa.net@gmail.com>
2020-01-28 09:32:01 +0100

    Sign out link

--- client/src/App.js
+++ client/src/App.js
@@ -21,11 +21,11 @@ flex: 0 0 auto;
 
 display: flex;
 background: #00aecc;
-overflow: hidden;
 padding: 4px ;
 justify-content: space-between;
 align-items: center;
 color:#fff;
+padding: 0 140px;
 `
 
 const Title = styled.div`
@@ -37,22 +37,44 @@ const Left = styled.div`
 `
 
 const Right = styled.div`
+  position: relative;
 `
 
-const App = () => <AppC>
-  <Header>
-    <Left></Left>
-    <Title> <Link to='/' className="title">CardsInRacks</Link></Title>
-    <Right></Right>
+class App extends React.Component {
 
-  </Header>
-  <Switch>
-    <Route path='/' exact component={Base} />
-    <Route path='/boards/:id' component={BoardAuth} />
-    <Route path="/signin" component={SignIn} />
-    <Route path="/signup" component={SignUp} />
-    <Route component={NotFound} />
-  </Switch>
-</AppC>
+  state = { loggedIn: true };
+
+  onClickHandler(e) {
+    e.preventDefault();
+    localStorage.removeItem('user');
+    this.setState({ loggedIn: false });
+  }
+
+  render() {
+    const user = JSON.parse(localStorage.getItem('user'))
+    return <AppC>
+      <ul className='header'>
+        <li className='left'></li>
+        <li className='title'> <Link to='/' className='title'>CardsInRacks</Link></li>
+        <li className='right'> {user ? "Hi " + user['name'] : ''}
+        <ul className='nav'>
+            <li className="row">
+              <a href="#" onClick={this.onClickHandler.bind(this)}>Sign Out</a>
+            </li>
+        </ul>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path='/' exact component={Base} />
+        <Route path='/boards/:id' component={BoardAuth} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route component={NotFound} />
+      </Switch>
+    </AppC>
+  }
+
+}
 
 export default App;
